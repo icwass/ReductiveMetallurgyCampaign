0b96973278b3dfdbb528951561616adb26c76739
icwass <isaac.wass@hotmail.com>
2023-03-19 19:24:05 -0500

    repository setup

---
+++ ReductiveMetallurgyCampaign/ReductiveMetallurgyCampaign.cs
@@ -0,0 +1,26 @@
+﻿using MonoMod.RuntimeDetour;
+using MonoMod.Utils;
+using Quintessential;
+using Quintessential.Settings;
+using SDL2;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace ReductiveMetallurgyCampaign;
+
+//using PartType = class_139;
+//using Permissions = enum_149;
+//using BondType = enum_126;
+//using BondSite = class_222;
+//using AtomTypes = class_175;
+//using PartTypes = class_191;
+//using Texture = class_256;
+public class MainClass : QuintessentialMod
+{
+	public override void Load() { }
+	public override void LoadPuzzleContent() { }
+	public override void Unload() { }
+	public override void PostLoad() { }
+}
